f16c167337a26f748cf43094e5dcbefb64d508d6
Malakhov Nikita <shadowkzn@gmail.com>
2018-12-06 23:31:13 +0300

    fix report

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -3,25 +3,35 @@
 namespace App\Http\Controllers;
 
 use App\Models\Activities\Activity;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ActivityController extends Controller
 {
-
-    public function __construct()
-    {
-    }
-
     /**
      * @param Request $request
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function index(Request $request)
     {
+        $start_date = $request->get('start_date');
+        $finish_date = $request->get('finish_date');
+
+
+        $startDate = $this->getStartOfDay($start_date);
+        $finishDate  = $this->getEndOfDay($finish_date);
+        $finishStatement = $finish_date
+            ? "COALESCE(if(unix_timestamp(finish_date) < unix_timestamp('$finishDate'), unix_timestamp(finish_date), unix_timestamp('$finishDate')), unix_timestamp())"
+            : "COALESCE(unix_timestamp(finish_date), unix_timestamp())";
+        $startStatement = $start_date
+            ? "if(unix_timestamp(start_date) > unix_timestamp('$startDate'), unix_timestamp(start_date), unix_timestamp('$startDate'))"
+            : "unix_timestamp(start_date)";
+        $rawQuery = "sum($finishStatement - $startStatement) as seconds";
+
         $query = Activity::groupBy('assortment_id')
             ->groupBy('color')
             ->with('assortment')
-            ->selectRaw('assortment_id, color, sum(COALESCE(unix_timestamp(finish_date), unix_timestamp()) - unix_timestamp(start_date)) as time');
+            ->selectRaw("assortment_id, color, $rawQuery");
 
         $shop_id = $request->get('shop_id');
         if ($shop_id) {
@@ -30,23 +40,40 @@ class ActivityController extends Controller
             });
         }
 
-        $start_date = $request->get('start_date');
         if ($start_date) {
             $query->where('start_date', '>', $start_date);
         }
 
-        $finish_date = $request->get('finish_date');
         if ($finish_date) {
             $query->where('start_date', '<', $finish_date);
         }
 
+        $query->orderBy('seconds', 'DESC');
         $activities = $query->get();
 
         $activities->map(function ($activity) {
-            $activity->time = $this->calculateTime($activity->time);
+            $activity->time = $this->calculateTime($activity->seconds);
         });
 
         return view('activities.index', compact('activities', 'shop_id', 'start_date', 'finish_date'));
+    }
+
+    /**
+     * @param $startDay
+     * @return null|string
+     */
+    private function getStartOfDay($startDay)
+    {
+        return $startDay ? (new Carbon($startDay))->endOfDay()->toDateTimeString() : null;
+    }
+
+    /**
+     * @param $endDay
+     * @return null|string
+     */
+    private function getEndOfDay($endDay)
+    {
+        return $endDay ? (new Carbon($endDay))->endOfDay()->toDateTimeString() : null;
     }
 
     /**
@@ -56,7 +83,7 @@ class ActivityController extends Controller
     private function calculateTime($time)
     {
         $hours = floor($time / 3600);
-        $minutes = floor($time / 3600) % 60;
+        $minutes = floor($time / 60 % 60);
         $seconds = $time % 60;
         return sprintf("%s:%02d:%02d", $hours, $minutes, $seconds);
     }
